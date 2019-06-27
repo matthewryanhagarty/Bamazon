b3d7dbd4500b0f5d14b4955fb6b61a48b3606c2d
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-06-26 22:45:40 -0700

    working shopping my functions because they are not working the way that i would like them to. creating for loop to iterate through table.

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
+var newStock;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,45 +14,45 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-var newStock;
 
-connection.query("SELECT * FROM products", function(err, res) {
+connection.query("SELECT * FROM products", function (err, res) {
   console.log("connected as id " + connection.threadId);
+  if (err) throw err;
+  printAll();
+});
+
+
+function printAll() {
+
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    printAll();
+
+    var table = new Table({
+      head: ["ID", "Product Name", "Department", "Price", "Stock Quantity"],
+      colWidths: [10, 40, 20, 10, 20]
+    })
+    for (var i = 0; i < res.length; i++) {
+      table.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        res[i].price,
+        res[i].stock_quantity,
+      ]);
+    }
+    newStock = res[i].stock_quantity;
+    console.log(table.toString());
+    userPrompt(newStock);
   });
+}
 
-
-  function printAll() {
-
-    connection.query("SELECT * FROM products", function (err, res) {
-      if (err) throw err;
-
-      var table = new Table({
-        head: ["ID", "Product Name", "Department", "Price", "Stock Quantity"],
-        colWidths: [10, 40, 20, 10, 20]
-      })
-      for (var i = 0; i < res.length; i++) {
-        table.push([
-          res[i].item_id,
-          res[i].product_name,
-          res[i].department_name,
-          res[i].price,
-          res[i].stock_quantity,
-        ]);
-      }
-      console.log(table.toString());
-      userPrompt();
-    });
-  }
-
-  function userPrompt(newStock) {
-    inquirer.prompt([
-      {
+function userPrompt(newStock) {
+  inquirer.prompt([
+    {
       name: "id",
       type: "input",
       message: "What is the ID of the Item you would like to purchase?",
-      validate: function(value) {
+      validate: function (value) {
         if (isNaN(value) === false) {
           return true;
         }
@@ -62,7 +63,7 @@ connection.query("SELECT * FROM products", function(err, res) {
       name: "quantity",
       type: "input",
       message: "How many would you like to purchase?",
-      validate: function(value) {
+      validate: function (value) {
         if (isNaN(value) === false) {
           return true;
         }
@@ -70,51 +71,60 @@ connection.query("SELECT * FROM products", function(err, res) {
       }
     },
   ])
-    
-    .then(function(answer){
 
-      var query = connection.query(
+    .then(function (answer) {
 
-        console.log(answer),
 
-        "UPDATE products SET ? WHERE ?", 
-        [
-          {
-            stock_quantity: newStock
-          },
-          {
-            item_id: answer.id
-          }
-        ],
-    function(err) {
-    if (err) throw err;
-    printAll();
-    }
-        );
+
+      newStock = stock_quantity - answer.quantity
+
+      updateProduct(newStock)
 
     }
     )
-    
+
+}
+
+
+
+
+
+  function updateProduct(newStock) {
+    connection.query(
+
+      "UPDATE products SET ? WHERE ?", 
+      [
+        {
+          stock_quantity: newStock
+        },
+        {
+          item_id: answer.id
+        }
+      ],
+  function(err, res) {
+         if (err) throw err;
+  printAll();
+  }
+      );
   }
 
-  
 
 
+  // connection.query(
 
-  // function updateProduct() {
-  //   connection.query(
+  //   console.log(answer),
 
-  //     "UPDATE products SET ? WHERE ?", 
-  //     [
-  //       {
-  //         stock_quantity: stock_quantity - answer.quantity
-  //       },
-  //       {
-  //         item_id: answer.id
-  //       }
-  //     ],
-  // function(err, res) {
-  // printAll();
-  // }
-  //     );
-  // }
+  //   "UPDATE products SET ? WHERE ?",
+  //   [
+  //     {
+  //       stock_quantity: newStock
+  //     },
+  //     {
+  //       item_id: answer.id
+  //     }
+  //   ],
+  //   function (err) {
+  //     if (err) throw err;
+  //     printAll();
+  //   }
+  // );

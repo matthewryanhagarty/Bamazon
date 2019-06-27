78b9d213c3712f0c61110ef8a66b562b19e8e494
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-06-26 18:10:44 -0700

    got cli-table to work and display in terminal.

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -18,5 +19,27 @@ var connection = mysql.createConnection({
 connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    console.log(res);
+    printAll();
   });
+
+  function printAll() {
+
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+
+      var table = new Table({
+        head: ["ID", "Product Name", "Department", "Price", "Stock Quantity"],
+        colWidths: [10, 40, 20, 10, 20]
+      })
+      for (var i = 0; i < res.length; i++) {
+        table.push([
+          res[i].item_id,
+          res[i].product_name,
+          res[i].department_name,
+          res[i].price,
+          res[i].stock_quantity,
+        ]);
+      }
+      console.log(table.toString());
+    });
+  }
